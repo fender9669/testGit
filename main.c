bd8c4aa2f800bab47cc8b9c1570e0488fa33ad9e
Dragan Vlad <vlad.dragan96@icloud.com>
2021-08-01 23:35:02 +0300

    changed main.c

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include main.h
 
+static int sum = 0;
+
+void incrementSum(){
+    sum += 1;
+}
+
 int main(){
 
     printf("Hello World!");
+
+   int i = 0;
+   while(i < 10){
+       incrementSum();
+       i++;
+   }
+
+   printf("Sum = %d", sum);
+
 
     return 0;
 }
